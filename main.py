14ead5e5237294fae1c0cd9abf50ab2202597ce5
shahuljava <shahulshaik2781@gmail.com>
2025-01-18 07:44:11 -0700

    procfile added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import router  # Assuming `router` is where your API routes are defined
+import os
+from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +17,11 @@ app.add_middleware(
 
 app.include_router(router)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+if __name__ == "__main__":
+    # Get the port from the environment variable or use 8000 for local testing
+    port = int(os.environ.get("PORT", 3000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
